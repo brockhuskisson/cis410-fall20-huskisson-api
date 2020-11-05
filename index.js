1b4ce286d5523507073cb32bb0ebae4ae7c4f0aa
brockhuskisson <bhusk@rams.colostate.edu>
2020-11-05 16:16:21 -0700

    10.1 10/27 /movies/:pk

--- index.js
+++ index.js
@@ -19,6 +19,37 @@ app.get("/movies", (req,res)=>{
     .then((result)=>{
         res.status(200).send(result)
     })
+    .catch((err)=>{
+        console.log(err)
+        res.status(500).send()
+    })
 })
 
+app.get("/movies/:pk", (req, res)=> {
+    var pk = req.params.pk
+    
+    var  myQuery = `SELECT *
+    FROM movie
+    LEFT JOIN genre
+    ON genre.GenrePK = movie.GenreFK
+    WHERE moviePK = ${pk}`
+
+    console.log(myQuery)
+
+    db.executeQuery(myQuery)
+        .then((movies)=>{
+            // console.log("Movies: ", movies)
+            if(movies[0]){
+                res.send(movies[0])
+            }
+            else{res.status(404).send('bad request')}
+            
+        })
+        .catch((err)=>{
+            console.log("Error in /movies/pk", err)
+            res.status(500).send()
+        })
+}
+
+)
 app.listen(5000,()=>{console.log("app is running on port 5000")})
